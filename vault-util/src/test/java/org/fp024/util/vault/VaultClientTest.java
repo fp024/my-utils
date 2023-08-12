fad332473b3c06ed6e481e60de46368d6694ed0f
fp024 <aopenasus@naver.com>
2023-08-12 19:15:46 +0900

    vault 토큰은 항상 사용자 홈 경로에서 읽도록 수정
    vault 토큰이 담긴 프로퍼티 파일을 만들고 .gitignore 처리하는게 오히려 실수를 할 여지가 많아 보여서,
    
    https://github.com/spring-cloud/spring-cloud-vault/blob/a588534dfe1269d48212b511bb178d76ff4c453c/spring-cloud-vault-config/src/main/java/org/springframework/cloud/vault/config/ClientAuthenticationFactory.java#L413
    
    위의 코드 참고해서, 무조건 사용자 홈 경로의 .vault-token 파일을 읽어 오도록 수정.

--- vault-util/src/test/java/org/fp024/util/vault/VaultClientTest.java
+++ vault-util/src/test/java/org/fp024/util/vault/VaultClientTest.java
@@ -2,30 +2,13 @@ package org.fp024.util.vault;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.io.IOException;
-import java.util.Properties;
 import org.junit.jupiter.api.Test;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.support.PropertiesLoaderUtils;
 
 class VaultClientTest {
-
   @Test
-  void test() throws IOException {
+  void testDefault() {
 
-    Properties prop =
-        PropertiesLoaderUtils.loadProperties(new ClassPathResource("vault.properties"));
-    Properties tokenProp =
-        PropertiesLoaderUtils.loadProperties(new ClassPathResource("vault-token.properties"));
-
-    VaultConfig config =
-        new VaultConfig(
-            prop.getProperty("vault.server.url"), //
-            tokenProp.getProperty("vault.policy.token"),
-            prop.getProperty("vault.root.path"),
-            prop.getProperty("vault.project.path"));
-
-    VaultClient client = new VaultClient(config);
+    VaultClient client = new VaultClient();
 
     String username = client.get("db.username");
     String password = client.get("db.password");
@@ -39,17 +22,5 @@ class VaultClientTest {
 
     assertThat(intValue).isEqualTo(1);
     assertThat(longValue).isEqualTo(2147483648L);
-  }
-
-  @Test
-  void testDefault() {
-
-    VaultClient client = new VaultClient();
-
-    String username = client.get("db.username");
-    String password = client.get("db.password");
-
-    assertThat(username).isEqualTo("db_username");
-    assertThat(password).isEqualTo("db_password");
   }
 }
