7c8f955bbf7d590237eee88ceda682fa4eeff4c1
fp024 <aopenasus@naver.com>
2023-08-13 20:24:14 +0900

    테스트 메서드 초기화 방법 변경

--- my-test-util/src/test/java/org/fp024/test/util/fixeddate/JodaTimeTests.java
+++ my-test-util/src/test/java/org/fp024/test/util/fixeddate/JodaTimeTests.java
@@ -2,27 +2,26 @@ package org.fp024.test.util.fixeddate;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import lombok.extern.slf4j.Slf4j;
 import org.joda.time.DateTimeUtils;
 import org.joda.time.LocalDateTime;
 import org.joda.time.format.DateTimeFormat;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 /*
    예전 Java 6 프로젝트 환경에서는 Java 프로젝트의 날짜 처리에 JodaTime의 클래스를 썻었음.
     JodaTime 을 사용할 때는 Mockito를 사용할 필요가 없어서 좋긴했음. 😅
 */
-@Slf4j
 class JodaTimeTests {
-  private final LocalDateTime dateTime =
+  private static final LocalDateTime dateTime =
       new LocalDateTime() //
           .withDate(2023, 8, 13)
           .withTime(1, 2, 3, 4);
 
-  @BeforeEach
-  void beforeEach() {
+  // 고정값으로 설정한 시간이 그 시점부터 흐르는게 아니고, 말대그대로 고정이기 때문에, 한번만 초기화해도 될 것 같다.
+  @BeforeAll
+  static void beforeAll() {
     // 테스트 날짜 고정
     DateTimeUtils.setCurrentMillisFixed(dateTime.toDate().getTime());
   }
@@ -36,8 +35,8 @@ class JodaTimeTests {
     assertThat(dateTimeString).isEqualTo("2023-08-13 01:02:03.004");
   }
 
-  @AfterEach
-  void afterEach() {
+  @AfterAll
+  static void afterAll() {
     // 테스트 메서드 끝나면 날짜 원상 복구
     DateTimeUtils.setCurrentMillisSystem();
   }
