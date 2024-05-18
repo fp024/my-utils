be4bb1f0a54d56b32fd8ea0389e8c346ae75e88f
fp024 <aopenasus@naver.com>
2024-05-18 14:50:11 +0900

    다른 스레드에서 Mock Static이 동작하지 않음을 확인하는 테스트 작성
    예전에 별도 Reactive예제로 확인해보긴 했는데..
    테스트 코드로 간단하게 확인해보기로 했다.

--- my-test-util/src/test/java/org/fp024/test/util/fixeddate/FixedDateExtensionUsingTests.java
+++ my-test-util/src/test/java/org/fp024/test/util/fixeddate/FixedDateExtensionUsingTests.java
@@ -1,10 +1,14 @@
 package org.fp024.test.util.fixeddate;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.FutureTask;
 import lombok.extern.slf4j.Slf4j;
 import org.fp024.test.util.fixeddate.extension.FixedDateExtension;
 import org.fp024.test.util.fixeddate.extension.annotation.FixedLocalDate;
@@ -74,5 +78,67 @@ class FixedDateExtensionUsingTests {
   void testFixedLocalTime03() {
     assertThat(LocalTime.now()) //
         .isEqualTo(LocalTime.of(1, 20));
+  }
+
+  /*
+    💡Thread가 다르면 Mocking이 안되는 것을 확인할 수 있는데,
+       아래의 테스트 코드는,
+       Runnable 블록 내에서 발생한 assertion예외가 밖으로 전파되지 않음.
+
+       JUnit은 기본적으로 메인스레드에서 테스트를 실행하고,
+       별도에 스레드에서 발생하는 예외는 JUnit에서 캐치되지 않음.
+       그러므로 별도의 스레드를 메인스레드로 전파해야됨.
+
+       FutureTask는 Runnable을 받아들이며, Runnable에서 발생하는 예외를 캡쳐하여,
+       나중에 메인스레드에서 확인할 수 있게함.
+  */
+  @FixedLocalTime(hour = 1, minute = 20)
+  @Test
+  void testFixedLocalTime_Other_Thread() throws Exception {
+    Runnable r =
+        () -> {
+          // 💡 검증 예외는 발생하지만 메인스레드로 전파되지 않음.
+          assertThat(LocalTime.now()) //
+              .isEqualTo(LocalTime.of(1, 20));
+        };
+
+    Thread t1 = new Thread(r);
+    t1.start();
+    t1.join();
+  }
+
+  @FixedLocalTime(hour = 1, minute = 20)
+  @Test
+  void testFixedLocalTime_Other_Thread_02() throws Exception {
+    FutureTask<Void> futureTask =
+        new FutureTask<>(
+            () -> {
+              assertThat(LocalTime.now()).isEqualTo(LocalTime.of(1, 20));
+              return null;
+            });
+
+    Thread t1 = new Thread(futureTask);
+    t1.start();
+    t1.join();
+
+    // 이 부분에서 별도의 스레드에서 발생한 예외를 확인하고 처리함.
+    assertThatThrownBy(() -> futureTask.get())
+        .hasCauseInstanceOf(org.opentest4j.AssertionFailedError.class);
+  }
+
+  @FixedLocalTime(hour = 1, minute = 20)
+  @Test
+  void testFixedLocalTime_Other_Thread_03() throws Exception {
+
+    Future<?> executor =
+        Executors.newSingleThreadExecutor()
+            .submit(
+                () -> {
+                  assertThat(LocalTime.now()) //
+                      .isEqualTo(LocalTime.of(1, 20));
+                });
+
+    assertThatThrownBy(() -> executor.get())
+        .hasCauseInstanceOf(org.opentest4j.AssertionFailedError.class);
   }
 }
