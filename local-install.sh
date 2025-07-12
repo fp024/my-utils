4ab6d8d27b3de01c6cda5c8a463d28ae006602a5
fp024 <aopenasus@naver.com>
2025-07-12 22:43:47 +0900

    linux용 설치 스크립트 개선
    for에서 while로 바꿔서 일반 sh에서도 실행가능해야한다. 😅

--- local-install.sh
+++ local-install.sh
@@ -1,18 +1,20 @@
-#!/bin/bash
+#!/bin/sh
 echo "copy library local install."
-source ./set-jdk-env.properties
 
 REPO_ROOT_DIR=$(pwd)
-PROJECT_FOLDER_LIST=$(sed 's/\\/\//g' "project-folder-list.txt")
+. "$REPO_ROOT_DIR/set-jdk-env.properties"
 
-
-for folder in $PROJECT_FOLDER_LIST;
-do
-  cd $REPO_ROOT_DIR/$folder
-  echo "##########################"
-  echo "$folder install"
-  echo "##########################"
-  ./gradlew clean test publishToMavenLocal
+# sed의 출력을 파이프로 while 루프에 연결, 마지막 출력 뒤에 빈줄 하나 추가해줌 "$a\"
+sed "s/\\/\//g" -e '$a\' "project-folder-list.txt" | while IFS= read -r folder; do
+  if [ -n "$folder" ]; then # 빈 줄 무시
+    cd "$REPO_ROOT_DIR/$folder" || { echo "ERROR: Failed to change directory to $REPO_ROOT_DIR/$folder" >&2; exit 1; }
+    echo "##########################"
+    echo "$folder install"
+    echo "##########################"
+    ./gradlew clean test publishToMavenLocal || { echo "ERROR: gradlew command failed for $folder" >&2; exit 1; }
+  fi
 done
+
+cd "$REPO_ROOT_DIR"
 
 echo "install is complete."
