b77a8f9b04fc4784b4c8813731c9e0d67ef3b4da
fp024 <aopenasus@naver.com>
2025-07-12 22:29:06 +0900

    라이브러리 업데이트, 로컬 설치 배치파일 동작 수정
    - vault-util-2x는 아직 JDK 8을 지원해야해서, log4j 관련 버전을 분리했다.
      log4j 2.25.0 부터는 최소 요구사항이 JDK11로 상향되었다.

--- local-install.bat
+++ local-install.bat
@@ -1,14 +1,17 @@
 @ECHO copy library local install.
 @ECHO OFF
+
 @CALL .\set-jdk-env.bat
 SET REPO_ROOT_DIR=%~dp0
+
 for /F "delims=" %%a in (project-folder-list.txt) do (
-  cd %REPO_ROOT_DIR%\%%a
+  @cd %REPO_ROOT_DIR%\%%a
   @ECHO ##########################
   @ECHO %%a install
   @ECHO ##########################
-  gradlew clean test publishToMavenLocal
+  @CALL gradlew clean test publishToMavenLocal  
 )
 
+@cd %REPO_ROOT_DIR%
 @ECHO install is complete.
 PAUSE
